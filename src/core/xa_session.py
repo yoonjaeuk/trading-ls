bc978c18cfff99302439c64da4f7f61d53c6de19
yoonjaeuk <jasper09@ajou.ac.kr>
2025-03-07 18:17:07 +0900

    Feat:xa-session

---
+++ src/core/xa_session.py
@@ -0,0 +1,13 @@
+import win32com.client
+import pythoncom
+import sys
+
+
+class XASessionReceiver:
+    def __init__(self):
+        pass
+
+
+class XASession:
+    def __init__(self):
+        pass
