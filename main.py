afc00cc2bc659735b9b0d701da9dd856847eb3a5
yoonjaeuk <jasper09@ajou.ac.kr>
2025-03-08 16:34:48 +0900

    Feat:login_setting

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
+from src.config.crypto_wallet import CRYPTO_WALLET
 from src.core.xa_session import XASession
 
 class Main:
     def __init__(self):
         #Settings
         login_server = "실투자"
-        
+        crypto_wallet = CRYPTO_WALLET[login_server]
         #Login
         xa_session = XASession(login_server=login_server)
         xa_session.connect_server()
